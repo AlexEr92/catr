8e295edff228f1dbe5f99215b7dd1718b4620e12
Alexander Eremenchuk <alexer92@yandex.ru>
2025-04-01 05:20:46 +0300

    test: test coverage improvement with invictus test

--- tests/catr-tests.rs
+++ tests/catr-tests.rs
@@ -10,6 +10,7 @@ const PRG: &str = "catr";
 const HELLO_WORLD_WITHOUT_NEWLINE: &str = "tests/inputs/hello_world_without_newline.txt";
 const HELLO_WORLD_WITH_NEWLINE: &str = "tests/inputs/hello_world_with_newline.txt";
 const COUNTING: &str = "tests/inputs/counting.txt";
+const INVICTUS: &str = "tests/inputs/invictus.txt";
 
 #[test]
 fn usage() -> TestResult {
@@ -72,4 +73,14 @@ fn counting() -> TestResult {
 #[test]
 fn counting_n() -> TestResult {
     run(&["--number", COUNTING], "tests/expected/counting.txt.n.out")
+}
+
+#[test]
+fn invictus() -> TestResult {
+    run(&[INVICTUS], "tests/expected/invictus.txt.out")
+}
+
+#[test]
+fn invictus_n() -> TestResult {
+    run(&["-n", INVICTUS], "tests/expected/invictus.txt.n.out")
 }
